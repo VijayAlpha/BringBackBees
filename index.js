cf522f9dbdb8395c3389a1bddb1e3b2273b09e80
Vijay M <vijaymurugan177@gmail.com>
2022-06-10 22:50:54 +0530

    UPDATE

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ document.querySelector(".nav-btn").addEventListener("click", (e) => {
   e.preventDefault();
   navEl.classList.toggle('navigation__open');
 });
+
+// document.addEventListener("mousemove" , function (e) {
+//  this.querySelectorAll('.header__primary-x').forEach(el => {
+//     const speed = el.getAttribute('data-speed');
+
+//     const x = (window.innerWidth - e.pageX*speed) / 100;
+//     const y = (window.innerHeight - e.pageY*speed) / 100;
+
+//     el.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    
+//     console.log('sdd');
+//  });
+
+// })
