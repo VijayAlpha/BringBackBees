ed5d41b39534b6c5cd6fcb0a9778f2a6169f2370
Vijay M <vijaymurugan177@gmail.com>
2022-06-16 19:21:58 +0530

    Minor changes

--- bluePrint.js
+++ bluePrint.js
@@ -5,12 +5,27 @@ function bluePrint(num = 1) {
     bluePrint(e.target.id[11]);
   });
 
-  document.querySelector(`#popup-close-btn-${num}`).addEventListener("click", (e) => {
-    document.querySelectorAll(".popup").forEach(el => {
-        el .style.display = "none";
-    })
-    document.getElementById("blue-print").style.display = "flex";
-  });
+  document
+    .querySelector(`#popup-close-btn-${num}`)
+    .addEventListener("click", (e) => {
+      document.querySelectorAll(".popup").forEach((el) => {
+        el.style.display = "none";
+      });
+      document.getElementById("blue-print").style.display = "flex";
+    });
 }
 
+function mobileView() {
+  const El = `<div class="blueprint__col blueprint-01-bg" id="blueprint-01">
+  <div class="blueprint__text" id="blueprint-01">EVENTS</div>
+</div>
+<div class="blueprint__col blueprint-05-bg" id="blueprint-05">
+<div class="blueprint__text" id="blueprint-05">Gallery</div>
+</div>
+`;
+
+  document.querySelector(".blueprint__mobile").insertAdjacentHTML("afterbegin", El)
+}
+
+//mobileView();
 bluePrint();
